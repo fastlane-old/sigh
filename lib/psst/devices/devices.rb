35f24bbc7ecd6b6b6ea339078fbf525b2a021f8c
Felix Krause <KrauseFx@gmail.com>
2015-04-03 19:11:54 +0200

    Added device list and device class

--- lib/psst/devices/devices.rb
+++ lib/psst/devices/devices.rb
@@ -1,11 +1,37 @@
 module FastlaneCore
   module Psst
-    class Psst
-      def list_devices
+
+    class Device < Struct.new(:client, :id, :name, :udid, :platform, :status)
+      # Parse the server response
+      def self.create(client, hash)
+        Device.new(
+          client,
+          hash['deviceId'],
+          hash['name'],
+          hash['deviceNumber'],
+          hash['devicePlatform'],
+          hash['status']
+        )
+      end
+
+      def to_s
+        [self.name, self.udid].join(" - ")
+      end
+    end
+
+
+    class Client
+      def devices
+        return @devices if @devices
+
         response = unzip(Excon.post(URL_LIST_DEVICES, 
           headers: { 'Cookie' => "myacinfo=#{@myacinfo}" },
           body: "teamId=#{@team_id}"))
-        Plist::parse_xml(response)
+        results = Plist::parse_xml(response)
+        
+        @devices = results['devices'].collect do |current|
+          Device.create(self, current)
+        end
       end
     end
   end
