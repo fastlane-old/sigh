00b8cf65072acb81a5827dc88d3505056ba8129c
Felix Krause <KrauseFx@gmail.com>
2015-04-04 11:35:58 +0200

    Added a list of all available types of certificates

---
+++ lib/psst/profile_types.rb
@@ -0,0 +1,39 @@
+module FastlaneCore
+  module Psst
+    # This class contains the codes used for the different types of profiles
+    class ProfileTypes
+      class Provisioning
+        def self.development
+          "5QPB9NHCEI"
+        end
+        def self.distribution
+          "R58UK2EWSO"
+        end
+      end
+
+      class Push
+        def self.development
+          "BKLRAVXMGM"
+        end
+        def self.production
+          "3BQKVH9I2X"
+        end
+      end
+
+      def self.all_profile_types
+        [
+          "5QPB9NHCEI", # Development Provisioning Profiles
+          "R58UK2EWSO", # Distribution Provisioning Profiles
+          "9RQEK7MSXA", # iOS Distribution certificate signing request
+          "LA30L5BJEU", # MDM CSR certificate signing request
+          "BKLRAVXMGM", # Development Push Certificates
+          "3BQKVH9I2X", # Production Push Certificates
+          "Y3B2F3TYSI", # Pass Type ID pass certificate request
+          "3T2ZP62QW8", # Website Push Id
+          "E5D663CMZW", # Website Push Id
+          "4APLUP237T"  # Apple Pay
+        ]
+      end
+    end
+  end
+end
