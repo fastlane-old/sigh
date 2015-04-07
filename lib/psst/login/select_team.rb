42c2c3fc3a4e1f41264aa314c3bcd8635082f5e4
Felix Krause <KrauseFx@gmail.com>
2015-04-07 19:31:26 +0200

    Added example login server response

--- lib/psst/login/select_team.rb
+++ lib/psst/login/select_team.rb
@@ -27,4 +27,39 @@ module FastlaneCore
       end
     end
   end
-end
+endend
+
+# Example response
+# {"teams"=>
+#   [{"status"=>"active",
+#     "name"=>"SunApps GmbH",
+#     "teamId"=>"5A997XSAAA",
+#     "type"=>"Company/Organization",
+#     "extendedTeamAttributes"=>{},
+#     "teamAgent"=>{"teamMemberId"=>"5Y354CXU3A", "personId"=>1553424542, "firstName"=>"Felix", "lastName"=>"Krause", "email"=>"felix@krausefx.com", "developerStatus"=>"active"},
+#     "memberships"=>
+#      [{"membershipId"=>"HJ5WHYC5CE",
+#        "membershipProductId"=>"ds1",
+#        "name"=>"iOS Developer Program",
+#        "status"=>"active",
+#        "inDeviceResetWindow"=>false,
+#        "inRenewalWindow"=>false,
+#        "dateStart"=>#<DateTime: 2014-11-20T07:59:59+00:00 ((2456982j,28799s,0n),+0s,2299161j)>,
+#        "dateExpire"=>#<DateTime: 2015-11-20T07:59:59+00:00 ((2457347j,28799s,0n),+0s,2299161j)>,
+#        "platform"=>"ios",
+#        "availableDeviceSlots"=>100}],
+#     "currentTeamMember"=>
+#      {"teamMemberId"=>"HQR8N4G84W",
+#       "personId"=>1415355845,
+#       "firstName"=>"Felix",
+#       "lastName"=>"Krause",
+#       "email"=>"deliver@krausefx.com",
+#       "developerStatus"=>"active",
+#       "privileges"=>{},
+#       "roles"=>["TEAM_ADMIN"]}}],
+#  "creationTimestamp"=>"2015-04-07T14:13:46Z",
+#  "resultCode"=>0,
+#  "userLocale"=>"en_US",
+#  "protocolVersion"=>"QH65B2",
+#  "requestUrl"=>"https://developerservices2.apple.com:443//services/QH65B2/listTeams.action",
+#  "responseId"=>"d7fe23a8-7b13-45c9-8648-6a6ce6223848"}
