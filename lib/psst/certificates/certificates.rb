146f682b0afcb17a4d9afc8178fccf208dfb3819
Felix Krause <KrauseFx@gmail.com>
2015-04-07 14:25:18 +0200

    Improved certificate output

--- lib/psst/certificates/certificates.rb
+++ lib/psst/certificates/certificates.rb
@@ -19,7 +19,7 @@ module FastlaneCore
       end
 
       def to_s
-        [self.name, self.owner_type].join(" - ")
+        [self.name, self.owner_type, self.id].join(" - ")
       end
 
       # Examples
